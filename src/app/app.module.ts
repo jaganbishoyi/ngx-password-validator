bd3e15a648d949a1f20f237019f3c9579672d1c7
Jagan Mohan Bishoyi <jagan.bishoyi@people10.com>
2021-01-27 19:08:20 +0530

    Demo enhanced v1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-// import { NgPasswordValidatorOptions } from "projects/ng-password-validator/src/lib/ng-password-validator.interface";
-// import { NgPasswordValidatorModule } from "projects/ng-password-validator/src/lib/ng-password-validator.module";
-import { NgPasswordValidatorModule, NgPasswordValidatorOptions } from "ng-password-validator";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+import { NgPasswordValidatorOptions } from "projects/ng-password-validator/src/lib/ng-password-validator.interface";
+import { NgPasswordValidatorModule } from "projects/ng-password-validator/src/lib/ng-password-validator.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { DemoComponent } from "./demo/demo.component";
+// import { NgPasswordValidatorModule, NgPasswordValidatorOptions } from "ng-password-validator";
 
 
 export const MyDefaultOptions: NgPasswordValidatorOptions = {
@@ -18,9 +21,15 @@ export const MyDefaultOptions: NgPasswordValidatorOptions = {
         DemoComponent
     ],
     imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NgPasswordValidatorModule.forRoot(MyDefaultOptions as NgPasswordValidatorOptions)
+        BrowserModule, ReactiveFormsModule, AppRoutingModule,
+        NgPasswordValidatorModule.forRoot(MyDefaultOptions as NgPasswordValidatorOptions),
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(
+            {
+                timeOut: 4000,
+                preventDuplicates: true,
+            }
+        ),
     ],
     providers: [],
     bootstrap: [AppComponent]
