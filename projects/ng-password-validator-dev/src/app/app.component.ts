45ccee8750274e755a967fddf96b23c120f15712
Jagan Mohan Bishoyi <jagan.bishoyi@people10.com>
2022-07-09 20:20:12 +0530

    Dev env setup

--- projects/ng-password-validator-dev/src/app/app.component.ts
+++ projects/ng-password-validator-dev/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-password-validator-dev';
+  options = {
+    placement: 'bottom',
+    rules: {
+      password: {
+        type: 'range',
+        min: 6,
+        max: 10,
+      },
+    },
+    shadow: true,
+    offset: 15,
+  };
 }
